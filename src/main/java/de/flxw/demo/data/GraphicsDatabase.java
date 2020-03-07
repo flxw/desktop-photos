256d1a1c58910d4fcf0a550b42d368086c57198e
Felix Wolff <f.wolff@celonis.com>
2020-03-07 11:08:39 +0100

    Database class now spawns a thread to read DB at startup and do consistency check

--- src/main/java/de/flxw/demo/data/GraphicsDatabase.java
+++ src/main/java/de/flxw/demo/data/GraphicsDatabase.java
@@ -6,46 +6,75 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Repository
 public class GraphicsDatabase {
-    private ArrayList<String> db;
-    private static final Logger LOG =
-            LoggerFactory.getLogger(GraphicsDatabase.class);
+    protected List<String> db;
+    private final Logger LOG = LoggerFactory.getLogger(GraphicsDatabase.class);
+    private final String DB_NAME = "imagedb.java.serialized";
+    private final String[] supportedExtensions = {"png", "jpg"};
+
+    private Thread dbWorker;
 
     public GraphicsDatabase() {
     }
 
     @PostConstruct
-    public void refreshAfterStartup() {
-        LOG.info("Yeah, hello postconstruct event");
+    public void init() {
+        File dbFile = new File(DB_NAME);
+
+        if (dbFile.exists()) {
+            // read db file
+            // create diff filetree
+            // drop deleted
+            // add new
+            // update changed
+        } else {
+            LOG.info("No database found, populating and creating a new one");
+            dbWorker = new InitDbWorker();
+            dbWorker.run();
+        }
         /* spawn a thread here after the application has fully gone up.
            the thread should read in the DB file, and update its contents
            if the file did not exist, the thread should create it in the first place
          */
-        /*try (Stream<Path> walk = Files.walk(Paths.get("/Users/f.wolff/Pictures"))) {
-            // We want to find only regular files
-            this.db = walk
-                    .filter(Files::isRegularFile)
-                    .map(x -> x.toString())
-                    .filter(x -> x.endsWith("png"))
-                    .collect(Collectors.toList());
-
-            this.db.forEach(System.out::println);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
+        /**/
     }
 
     public List<String> getAllGraphics() {
         return db;
     }
+
+    private class InitDbWorker extends Thread {
+        public void run() {
+            String pwd = (new File("")).getAbsolutePath();
+
+            try (Stream<Path> walk = Files.walk(Paths.get(pwd))) {
+                db = walk
+                        .filter(Files::isRegularFile)
+                        .map(x -> x.toString())
+                        .filter(x -> this.isImage(x))
+                        .collect(Collectors.toList());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            LOG.info("InitDbWorker scanned files: " + db.size());
+        }
+
+        private boolean isImage(String fileName) {
+            return Arrays.stream(supportedExtensions).anyMatch(entry -> fileName.endsWith(entry));
+        }
+    }
+
 }
