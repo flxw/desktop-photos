d06b1049ed0a939b9eb985a60c7f95bbbec26e46
Felix Wolff <f.wolff@celonis.com>
2020-03-16 22:08:46 +0100

    Flexbox wrapping and virtual DOM

--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule }    from '@angular/common/http';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { HttpClientModule }    from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ScrollingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
