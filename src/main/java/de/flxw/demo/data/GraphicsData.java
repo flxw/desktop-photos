bfd6f55e95ac5d3d9e907e41f51b175c96fe0346
Felix Wolff <f.wolff@celonis.com>
2020-03-18 23:13:58 +0100

    #11 - refactor db population and include image dimensions

--- src/main/java/de/flxw/demo/data/GraphicsData.java
+++ src/main/java/de/flxw/demo/data/GraphicsData.java
@@ -1,8 +1,20 @@
 package de.flxw.demo.data;
 
+import com.drew.imaging.jpeg.JpegMetadataReader;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.ExifIFD0Directory;
+import com.drew.metadata.jpeg.JpegDirectory;
 import lombok.Getter;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.security.MessageDigest;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
@@ -11,19 +23,91 @@ import java.util.Objects;
 public class GraphicsData implements Serializable {
     @Getter private String fileName;
     @Getter private String checkSum;
-    @Getter private Date timeStamp;
+    @Getter private Date timeStamp = new Date(0,0,0);
     @Getter private boolean valid;
-
-    public GraphicsData(String fileName, String checkSum, Date timeStamp, boolean valid) {
-        this.fileName = fileName;
-        this.checkSum = checkSum;
-        this.timeStamp = timeStamp;
-        this.valid = valid;
-    }
+    private int height = 400;
+    private int width  = 800;
 
     public Date getDate() {
         Instant i = this.timeStamp.toInstant().truncatedTo(ChronoUnit.DAYS);
         return Date.from(i);
+    }
+
+    public static GraphicsData of(String fileName) {
+        File f = new File(fileName);
+        GraphicsData gd = new GraphicsData();
+
+        gd.fileName = fileName;
+        gd.checkSum = generateChecksum(f);
+        gd.valid = !gd.checkSum.equals("");
+        setPropertiesFromMetadata(f, gd);
+
+        return gd;
+    }
+
+    private static String generateChecksum(File file) {
+        String cs;
+
+        try {
+            MessageDigest md5Digest = MessageDigest.getInstance("MD5");
+            cs = getFileChecksum(md5Digest, file);
+        } catch (Exception e) {
+            cs = "";
+        }
+
+        return cs;
+    }
+
+    private static void setPropertiesFromMetadata(File file, GraphicsData graphicsData) {
+        try {
+            Metadata mt = JpegMetadataReader.readMetadata(file);
+            Directory exif = mt.getFirstDirectoryOfType(ExifIFD0Directory.class);
+
+            // extract time when the picture was taken
+            String s = exif.getString(ExifIFD0Directory.TAG_DATETIME);
+            SimpleDateFormat exifDateParser = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");
+            graphicsData.timeStamp = exifDateParser.parse(s);
+
+            // extract image dimensions
+            Directory jpeg = mt.getFirstDirectoryOfType(JpegDirectory.class);
+            graphicsData.height = jpeg.getInt(JpegDirectory.TAG_IMAGE_HEIGHT);
+            graphicsData.width  = jpeg.getInt(JpegDirectory.TAG_IMAGE_WIDTH);
+        } catch (Exception e) {}
+
+        try {
+            BasicFileAttributes attr = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+            graphicsData.timeStamp = new Date(attr.creationTime().toMillis());
+        } catch (Exception e) {}
+    }
+
+    private static String getFileChecksum(MessageDigest digest, File file) throws IOException {
+        //Get file input stream for reading the file content
+        FileInputStream fis = new FileInputStream(file);
+
+        //Create byte array to read data in chunks
+        byte[] byteArray = new byte[1024];
+        int bytesCount;
+
+        //Read file data and update in message digest
+        while ((bytesCount = fis.read(byteArray)) != -1) {
+            digest.update(byteArray, 0, bytesCount);
+        };
+
+        //close the stream; We don't need it now.
+        fis.close();
+
+        //Get the hash's bytes
+        byte[] bytes = digest.digest();
+
+        //This bytes[] has bytes in decimal format;
+        //Convert it to hexadecimal format
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i< bytes.length ;i++) {
+            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        //return complete hash
+        return sb.toString();
     }
 
     @Override
--- src/main/java/de/flxw/demo/data/GraphicsDatabase.java
+++ src/main/java/de/flxw/demo/data/GraphicsDatabase.java
@@ -1,9 +1,5 @@
 package de.flxw.demo.data;
 
-import com.drew.imaging.jpeg.JpegMetadataReader;
-import com.drew.metadata.Directory;
-import com.drew.metadata.Metadata;
-import com.drew.metadata.exif.ExifIFD0Directory;
 import de.flxw.demo.Configuration;
 import lombok.Getter;
 import org.slf4j.Logger;
@@ -15,11 +11,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.security.MessageDigest;
-import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -84,77 +76,6 @@ public class GraphicsDatabase {
                     .stream(Configuration.SUPPORTED_EXTENSIONS)
                     .anyMatch(entry -> fileName.endsWith(entry));
         }
-
-        protected GraphicsData createGraphicsObject(String fileName) {
-            File f = new File(fileName);
-
-            String cs = generateChecksum(f);
-            Date time = extractTimeTaken(f);
-            boolean valid = !cs.equals("");
-
-            return new GraphicsData(fileName, cs, time, valid);
-        }
-
-        private String generateChecksum(File file) {
-            String cs;
-
-            try {
-                MessageDigest md5Digest = MessageDigest.getInstance("MD5");
-                cs = getFileChecksum(md5Digest, file);
-            } catch (Exception e) {
-                cs = "";
-            }
-
-            return cs;
-        }
-
-        private Date extractTimeTaken(File file) {
-            try {
-                Metadata mt = JpegMetadataReader.readMetadata(file);
-                Directory exif = mt.getFirstDirectoryOfType(ExifIFD0Directory.class);
-                String s = exif.getString(ExifIFD0Directory.TAG_DATETIME);
-                SimpleDateFormat exifDateParser = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");
-                Date d = exifDateParser.parse(s);
-                return d;
-            } catch (Exception e) {}
-
-            try {
-                BasicFileAttributes attr = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
-                return new Date(attr.creationTime().toMillis());
-            } catch (Exception e) {}
-
-            return new Date(0,0,0);
-        }
-
-        private String getFileChecksum(MessageDigest digest, File file) throws IOException {
-            //Get file input stream for reading the file content
-            FileInputStream fis = new FileInputStream(file);
-
-            //Create byte array to read data in chunks
-            byte[] byteArray = new byte[1024];
-            int bytesCount;
-
-            //Read file data and update in message digest
-            while ((bytesCount = fis.read(byteArray)) != -1) {
-                digest.update(byteArray, 0, bytesCount);
-            };
-
-            //close the stream; We don't need it now.
-            fis.close();
-
-            //Get the hash's bytes
-            byte[] bytes = digest.digest();
-
-            //This bytes[] has bytes in decimal format;
-            //Convert it to hexadecimal format
-            StringBuilder sb = new StringBuilder();
-            for(int i=0; i< bytes.length ;i++) {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-
-            //return complete hash
-            return sb.toString();
-        }
     }
 
     private class InitDbWorker extends DbWorker {
@@ -167,7 +88,7 @@ public class GraphicsDatabase {
                         .filter(Files::isRegularFile)
                         .map(Path::toString)
                         .filter(this::isImage)
-                        .map(this::createGraphicsObject)
+                        .map(GraphicsData::of)
                         .filter(x -> x.isValid())
                         .collect(Collectors.toMap(
                                 GraphicsData::getFileName,
@@ -246,7 +167,7 @@ public class GraphicsDatabase {
         private int addNewEntriesToDb(final Set<String> currentFilenames, final Set<String> recoveredFilenames) {
             Map<String, GraphicsData> toBeAdded = currentFilenames.stream()
                     .filter(x -> !recoveredFilenames.contains(x))
-                    .map(this::createGraphicsObject)
+                    .map(GraphicsData::of)
                     .collect(Collectors.toMap(
                             GraphicsData::getFileName,
                             Function.identity()
