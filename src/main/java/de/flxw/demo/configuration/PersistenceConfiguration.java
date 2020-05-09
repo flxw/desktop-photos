9ee44f5b52ca73f7424828f399a47423b3d81678
Felix Wolff <f.wolff@celonis.com>
2020-05-10 01:00:59 +0200

    optimize imports

--- src/main/java/de/flxw/demo/configuration/PersistenceConfiguration.java
+++ src/main/java/de/flxw/demo/configuration/PersistenceConfiguration.java
@@ -3,7 +3,6 @@ package de.flxw.demo.configuration;
 import org.hibernate.dialect.H2Dialect;
 import org.hibernate.dialect.function.StandardSQLFunction;
 import org.hibernate.type.CustomType;
-import org.hibernate.type.StandardBasicTypes;
 import org.hibernate.type.Type;
 
 public class PersistenceConfiguration extends H2Dialect {
