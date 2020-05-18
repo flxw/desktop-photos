8a32e3b28577a65e094e88c3732d8719c9ad16e1
Felix Wolff <f.wolff@celonis.com>
2020-05-18 23:09:56 +0200

    Reflow rows upon container width change

--- src/main/webapp/src/app/app.component.ts
+++ src/main/webapp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ViewChild, AfterViewChecked, ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, AfterViewChecked, ElementRef, HostListener } from '@angular/core';
 import { GraphicsService } from './graphics.service';
 import { Tile } from './tile';
 import { GraphicsData } from './graphics-data.object';
@@ -12,30 +12,69 @@ export class AppComponent implements AfterViewInit {
   @ViewChild("container") container;
   sampleTile:Tile = new Tile();
   timelineRows = [];
+  timelineElements:GraphicsData[] = [];
+  timelineContainerWidth:number;
 
-  constructor(public graphicsService: GraphicsService) {
-  }
+  lastResizeTime:number = null;
+  isResizeTimeoutRunning:boolean = false;
+  resizeTimeThreshold:number = 200;
+
+  constructor(public graphicsService: GraphicsService) { }
 
   ngAfterViewInit(): void {
-    //let that = this;
     this.graphicsService.getTimeline().subscribe((timelineElements:GraphicsData[]) => {
-      let rows = [];
-      const viewWidth = this.container.elementRef.nativeElement.clientWidth;
-  
-      for (let i = 0, rowWidth = 0, row = []; i < timelineElements.length; ++i) {
-        let itemWidth = GraphicsData.getScaledWidthForHeight(Tile.initialHeight, timelineElements[i]);
-        
-        if (rowWidth + itemWidth < viewWidth) {
-          rowWidth += itemWidth;
-          row.push(timelineElements[i]);
-        } else {
-          rows.push(row);
-          rowWidth = itemWidth;
-          row = [timelineElements[i]];
-        }
-      }
-  
-      this.timelineRows = rows;
+      this.timelineElements = timelineElements;
+      this.recalculateTimelineRows();
     });
+  }
+
+  recalculateTimelineRows():void {
+    let rows = [];
+    this.timelineContainerWidth = this.container.elementRef.nativeElement.clientWidth;
+
+    for (let i = 0, rowWidth = 0, row = []; i < this.timelineElements.length; ++i) {
+      let itemWidth = GraphicsData.getScaledWidthForHeight(Tile.initialHeight, this.timelineElements[i]);
+      
+      if (rowWidth + itemWidth < this.timelineContainerWidth) {
+        rowWidth += itemWidth;
+        row.push(this.timelineElements[i]);
+      } else {
+        rows.push(row);
+        rowWidth = itemWidth;
+        row = [this.timelineElements[i]];
+      }
+    }
+
+    this.timelineRows = rows;
+  }
+
+  @HostListener("window:resize", ["$event"]) onContainerResize(e:UIEvent) {
+    this.lastResizeTime = new Date().getMilliseconds();
+
+    if (this.isResizeTimeoutRunning === false) {
+      this.isResizeTimeoutRunning = true;
+      this.timeoutResizeEnd();
+    }
+  }
+  
+  handleResizeEnd() {
+    let now = new Date().getMilliseconds();
+    if ((now - this.lastResizeTime) < this.resizeTimeThreshold) {
+      this.timeoutResizeEnd();
+    } else {
+      this.isResizeTimeoutRunning = false;
+
+      let newWidth = this.container.elementRef.nativeElement.clientWidth;
+      
+      if (this.timelineContainerWidth != newWidth) {
+        console.log("Reflowing!")
+        this.recalculateTimelineRows();
+      }
+    }
+  }
+
+  timeoutResizeEnd() {
+    let thisCapsule = () => { this.handleResizeEnd() };
+    setTimeout(thisCapsule, this.resizeTimeThreshold);
   }
 }
